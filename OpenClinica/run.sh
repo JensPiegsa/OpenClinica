25024aaf0efb75939dcc583274cb797f3ba7e689
Jens Piegsa <piegsa@gmail.com>
2015-01-09 16:25:55 +0100

    Update run.sh

--- OpenClinica/run.sh
+++ OpenClinica/run.sh
@@ -9,4 +9,10 @@ sed -i "/^dbPort=.*/c\dbPort=$POSTGRES_PORT_5432_TCP_PORT" /tomcat/webapps/OpenC
 sed -i "/^dbHost=.*/c\dbHost=$POSTGRES_PORT_5432_TCP_ADDR" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
 sed -i "/^dbPort=.*/c\dbPort=$POSTGRES_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
 
+echo "org.apache.catalina.core.ContainerBase.[Catalina].level = INFO" > /tomcat/webapps/OpenClinica/WEB-INF/classes/logging.properties
+echo "org.apache.catalina.core.ContainerBase.[Catalina].handlers = java.util.logging.ConsoleHandler" >> /tomcat/webapps/OpenClinica/WEB-INF/classes/logging.properties
+
+echo "org.apache.catalina.core.ContainerBase.[Catalina].level = INFO" > /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/logging.properties
+echo "org.apache.catalina.core.ContainerBase.[Catalina].handlers = java.util.logging.ConsoleHandler" >> /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/logging.properties
+
 exec ${CATALINA_HOME}/bin/catalina.sh run
