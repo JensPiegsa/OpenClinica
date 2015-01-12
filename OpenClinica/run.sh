79575bc5102f7f58843582a1111e50b4922687c2
Jens Piegsa <piegsa@gmail.com>
2015-01-12 19:32:45 +0100

    Update run.sh

--- OpenClinica/run.sh
+++ OpenClinica/run.sh
@@ -3,11 +3,11 @@ if [ ! -f /.tomcat_admin_created ]; then
   /create_tomcat_admin_user.sh
 fi
 
-sed -i "/^dbHost=.*/c\dbHost=$POSTGRES_PORT_5432_TCP_ADDR" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
-sed -i "/^dbPort=.*/c\dbPort=$POSTGRES_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
+sed -i "/^dbHost=.*/c\dbHost=db" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
+sed -i "/^dbPort=.*/c\dbPort=db" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
 
-sed -i "/^dbHost=.*/c\dbHost=$POSTGRES_PORT_5432_TCP_ADDR" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
-sed -i "/^dbPort=.*/c\dbPort=$POSTGRES_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
+sed -i "/^dbHost=.*/c\dbHost=$DB_PORT_5432_TCP_ADDR" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
+sed -i "/^dbPort=.*/c\dbPort=$DB_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
 
 echo "org.apache.catalina.core.ContainerBase.[Catalina].level = INFO" > /tomcat/webapps/OpenClinica/WEB-INF/classes/logging.properties
 echo "org.apache.catalina.core.ContainerBase.[Catalina].handlers = java.util.logging.ConsoleHandler" >> /tomcat/webapps/OpenClinica/WEB-INF/classes/logging.properties
