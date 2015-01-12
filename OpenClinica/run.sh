7c8b6cfd644d7f4838c49dc6c24e23d518dfc393
Jens Piegsa <piegsa@gmail.com>
2015-01-12 21:23:39 +0100

    Update run.sh

--- OpenClinica/run.sh
+++ OpenClinica/run.sh
@@ -3,10 +3,10 @@ if [ ! -f /.tomcat_admin_created ]; then
   /create_tomcat_admin_user.sh
 fi
 
-sed -i "/^dbHost=.*/c\dbHost=jdbc:postgresql://db" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
+sed -i "/^dbHost=.*/c\dbHost=ocdb" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
 sed -i "/^dbPort=.*/c\dbPort=$DB_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
 
-sed -i "/^dbHost=.*/c\dbHost=jdbc:postgresql://db" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
+sed -i "/^dbHost=.*/c\dbHost=ocdb" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
 sed -i "/^dbPort=.*/c\dbPort=$DB_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
 
 echo "org.apache.catalina.core.ContainerBase.[Catalina].level = INFO" > /tomcat/webapps/OpenClinica/WEB-INF/classes/logging.properties
