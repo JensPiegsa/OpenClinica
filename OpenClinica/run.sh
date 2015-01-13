c51729432d07086a5827a9f1858eed52e8037f79
Jens Piegsa <piegsa@gmail.com>
2015-01-13 10:47:40 +0100

    Update run.sh

--- OpenClinica/run.sh
+++ OpenClinica/run.sh
@@ -4,10 +4,10 @@ if [ ! -f /.tomcat_admin_created ]; then
 fi
 
 sed -i "/^dbHost=.*/c\dbHost=ocdb" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
-sed -i "/^dbPort=.*/c\dbPort=$DB_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
+sed -i "/^dbPort=.*/c\dbPort=$OCDB_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
 
 sed -i "/^dbHost=.*/c\dbHost=ocdb" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
-sed -i "/^dbPort=.*/c\dbPort=$DB_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
+sed -i "/^dbPort=.*/c\dbPort=$OCDB_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
 
 echo "org.apache.catalina.core.ContainerBase.[Catalina].level = INFO" > /tomcat/webapps/OpenClinica/WEB-INF/classes/logging.properties
 echo "org.apache.catalina.core.ContainerBase.[Catalina].handlers = java.util.logging.ConsoleHandler" >> /tomcat/webapps/OpenClinica/WEB-INF/classes/logging.properties
