9cff8c34aaf0623bfc5ce73785140050c06cfbf9
Jens Piegsa <piegsa@gmail.com>
2015-02-04 18:29:20 +0100

    Update run.sh

--- OpenClinica/run.sh
+++ OpenClinica/run.sh
@@ -6,8 +6,6 @@ fi
 
 sed -i "/^dbHost=.*/c\dbHost=ocdb" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
 sed -i "/^dbPort=.*/c\dbPort=$OCDB_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
-sed -i "/^dbHost=.*/c\dbHost=ocdb" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
-sed -i "/^dbPort=.*/c\dbPort=$OCDB_PORT_5432_TCP_PORT" /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/datainfo.properties
 sed -i "/^userAccountNotification=.*/c\userAccountNotification=admin" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
 sed -i "/^# rssUrl=.*/c\rssUrl=" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
 sed -i "/^# rssMore=.*/c\rssMore=" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
@@ -16,6 +14,7 @@ sed -i "/^# about\.text2=.*/c\about.text2=<a href=\"https://mosaic-greifswald.de
 sed -i "/^# supportURL=.*/c\supportURL=https://mosaic-greifswald.de/openclinica" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
 sed -i "/^collectStats=.*/c\collectStats=false" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
 sed -i "/^designerURL=.*/c\designerURL=" /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties
+cp /tomcat/webapps/OpenClinica/WEB-INF/classes/datainfo.properties /tomcat/webapps/OpenClinica-ws/WEB-INF/classes/
 
 if [ -z "$LOG_LEVEL" ]; then
   echo "Using default log level."
